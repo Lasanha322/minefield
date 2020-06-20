73a2b632d297ba4bf1baf553ba1e990d41876014
CauanNewton <63312517+CauanNewton@users.noreply.github.com>
2020-06-19 21:06:57 -0300

    começo da janela do main menu

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -8,9 +11,25 @@ import interfaces.ISettings;
 
 public class GUI extends JFrame implements IGUI {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public void imprimirMenuInicial() {
 		JPanel mainPanel = new JPanel();
+		JButton buttonPlay = new JButton();
+		JButton buttonSettings = new JButton();
+		JButton buttonExit = new JButton();
+		
+		this.setLayout(new BorderLayout());
+		add(mainPanel, BorderLayout.CENTER);
+		
+		setTitle("Minefield - Main Menu");
+		setSize(1000, 800);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 
 	@Override
---
+++ src/minorObjects/MenuButton.java
@@ -0,0 +1,24 @@
+package minorObjects;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class MenuButton extends JButton implements ActionListener {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	MenuButton() {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
