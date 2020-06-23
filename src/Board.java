379ff7ef830c05de10faae0b6c748a551d4aa577
Bruna <62315950+b194966@users.noreply.github.com>
2020-06-23 16:31:46 -0300

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -2,40 +2,12 @@ import interfaces.IBoard;
 import interfaces.ICell;
 
 public class Board implements IBoard {
-	private ICell[] Celulas;
-	
-	public void revelarCelula(ICell Celula) {
-		// TODO Auto-generated method stub
-
+	ICell [] Celulas;
+	Board(int Tamanho){
+		Celulas = new ICell[Tamanho];
+	}
+	public ICell [] getCell () {
+		return Celulas;
 	}
 
-	@Override
-	public void revelar4Celulas(ICell[] celulas) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void visualizarCelula(ICell Celula) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void colocarBomba(ICell Celula) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void colocarBombaInvisivel(ICell Celula) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public ICell descobrirGema() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
