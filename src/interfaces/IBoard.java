654879220d666ade93288e8ec0c0c7926692ae3f
Bruna <62315950+b194966@users.noreply.github.com>
2020-06-23 16:32:15 -0300

    Update IBoard.java

--- src/interfaces/IBoard.java
+++ src/interfaces/IBoard.java
@@ -1,10 +1,4 @@
 package interfaces;
-
 public interface IBoard {
-	void revelarCelula(ICell Celula);
-	void revelar4Celulas(ICell Celulas[]);
-	void visualizarCelula(ICell Celula);
-	void colocarBomba(ICell Celula);
-	void colocarBombaInvisivel(ICell Celula);
-	ICell descobrirGema();
+	public ICell [] getCell ();
 }
